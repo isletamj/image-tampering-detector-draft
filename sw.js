59071ab7a3a9b57a6f84901b3762c936e8753815
isletamj <mjs.isleta@gmail.com>
2023-01-21 23:30:35 +0800

    Link manifest and Added service worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Perform install steps
+const CACHE_NAME = 'my-cache-v1';
+let urlsToCache = [
+    "/",
+    "/index.html",
+    "/src/master.css",
+    "/src/index.js",
+    "/images/camera128.png",
+    "/images/camera256.png",
+    "/images/camera512.png"
+    ];
+
+self.addEventListener('install', function(event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['pigment'];
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
